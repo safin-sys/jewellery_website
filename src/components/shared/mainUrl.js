a4288fbb83cca68d9073224387e6080a87f9f038
safin-sys <mdsafinofficial@gmail.com>
2025-03-27 14:31:08 +0600

    feat: update API base URL to use production endpoint

--- src/components/shared/mainUrl.js
+++ src/components/shared/mainUrl.js
@@ -1,12 +1,11 @@
 const mainUrl = async (url) => {
-   const response = await fetch(`http://10.0.60.129:3000${url}`);
-   
-   if (!response.ok) {
-     throw new Error(`HTTP error! status: ${response.status}`);
-   }
-   
-   return await response.json();
- };
- 
- export default mainUrl;
- 
+  const response = await fetch(`https://jewellery-backend-u0ok.onrender.com${url}`);
+  
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  
+  return await response.json();
+};
+
+export default mainUrl;
